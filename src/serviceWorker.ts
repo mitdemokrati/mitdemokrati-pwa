f1ff7a660ad53a758250d7aadb99b591b638ff1f
andreasgoodstein <24245735+andreasgoodstein@users.noreply.github.com>
2025-11-08 15:15:06 +0100

    feat: remove service worker caching

--- src/serviceWorker.ts
+++ src/serviceWorker.ts
@@ -3,62 +3,60 @@ import 'regenerator-runtime/runtime';
 export default null;
 declare const self: ServiceWorkerGlobalScope;
 
-const CACHE_NAME = 'mitdemokrati-v1';
-const URLS_TO_CACHE = [self.location.origin];
+// const CACHE_NAME = 'mitdemokrati-v1';
+// const URLS_TO_CACHE = [self.location.origin];
 
-self.addEventListener('install', (event) =>
-  event.waitUntil(populateCacheOnInstall())
-);
+self.addEventListener('install', () => Promise.resolve());
 
-async function populateCacheOnInstall() {
-  const cache = await caches.open(CACHE_NAME);
+// async function populateCacheOnInstall() {
+//   const cache = await caches.open(CACHE_NAME);
 
-  return cache.addAll(URLS_TO_CACHE);
-}
+//   return cache.addAll(URLS_TO_CACHE);
+// }
 
-self.addEventListener('fetch', (event) => {
-  const { request } = event;
+// self.addEventListener('fetch', (event) => {
+//   const { request } = event;
 
-  const updatableCacheFirstFetchResponse = new Promise<Response>((resolve) => {
-    let resolved = false;
+//   const updatableCacheFirstFetchResponse = new Promise<Response>((resolve) => {
+//     let resolved = false;
 
-    // Resolve response from cache
-    caches.match(request).then((cacheResponse) => {
-      if (!resolved && cacheResponse) {
-        resolved = true;
-        resolve(cacheResponse);
-      }
-    });
+//     // Resolve response from cache
+//     caches.match(request).then((cacheResponse) => {
+//       if (!resolved && cacheResponse) {
+//         resolved = true;
+//         resolve(cacheResponse);
+//       }
+//     });
 
-    // Do not fetch from network when offline
-    if (!navigator.onLine) {
-      return;
-    }
+//     // Do not fetch from network when offline
+//     if (!navigator.onLine) {
+//       return;
+//     }
 
-    // Resolve response from network and update cache
-    fetch(request).then((fetchResponse) => {
-      if (shouldCacheResponse(fetchResponse)) {
-        cloneResponseToCache(request, fetchResponse);
-      }
+//     // Resolve response from network and update cache
+//     fetch(request).then((fetchResponse) => {
+//       if (shouldCacheResponse(fetchResponse)) {
+//         cloneResponseToCache(request, fetchResponse);
+//       }
 
-      if (!resolved) {
-        resolved = true;
-        resolve(fetchResponse);
-      }
-    });
-  });
+//       if (!resolved) {
+//         resolved = true;
+//         resolve(fetchResponse);
+//       }
+//     });
+//   });
 
-  event.respondWith(updatableCacheFirstFetchResponse);
-});
+//   event.respondWith(updatableCacheFirstFetchResponse);
+// });
 
-// Only save good responses to cache
-function shouldCacheResponse(response: Response) {
-  return response && response.status === 200 && response.type === 'basic';
-}
+// // Only save good responses to cache
+// function shouldCacheResponse(response: Response) {
+//   return response && response.status === 200 && response.type === 'basic';
+// }
 
-async function cloneResponseToCache(request: Request, response: Response) {
-  const responseToCache = response.clone();
+// async function cloneResponseToCache(request: Request, response: Response) {
+//   const responseToCache = response.clone();
 
-  const openCache = await caches.open(CACHE_NAME);
-  openCache.put(request, responseToCache);
-}
+//   const openCache = await caches.open(CACHE_NAME);
+//   openCache.put(request, responseToCache);
+// }
